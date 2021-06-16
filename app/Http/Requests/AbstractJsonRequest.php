321e34b06df40c3a1713528d478c7908a9ec6b61
aleksejem <alexeyhong10@gmail.com>
2021-06-16 22:34:44 +0500

    added abstract json request class.

---
+++ app/Http/Requests/AbstractJsonRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class AbstractJsonRequest extends FormRequest
+{
+    public function authorize() : bool
+    {
+        return true;
+    }
+
+    abstract public function getDto();
+
+    final protected function failedValidation(Validator $validator)
+    {
+        throw new ValidationException(
+            $validator,
+            response()->json($validator->errors(), 422)
+        );
+    }
+}
